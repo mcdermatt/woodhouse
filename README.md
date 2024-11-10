cdadd1e074c317f47ba301c4b1adcdb99c1ef826
mcdermatt <mcdermatt@gmail.com>
2024-11-10 11:38:35 -0500

    adding node for odrive velocity control from twist msg

--- README.md
+++ README.md
@@ -24,9 +24,13 @@ Run node for joystick input
 
 ```rosrun joy joy_node```
 
+Run node to convert button output to twist message
+
 ```rosparam load $(rospack find woodhouse)/config/joy_config.yaml /teleop_twist_joy```
 
 ```rosrun teleop_twist_joy teleop_node```
+
+``` python3 cmd_vel_to_odrive.py```
 
 # TODO
 
@@ -35,8 +39,10 @@ Run node for joystick input
 
 #### Basic motion
 
-* [ ] Convert controller input to twist msg
-* [ ] convert twist to odrive command
+* [X] Convert controller input to twist msg
+* [X] Convert twist to odrive command
+* [ ] Fine tune motor gains
+* [ ] Tune turning radius
 
 #### Mapping and Localization
 
@@ -47,11 +53,17 @@ Run node for joystick input
 
 #### Hardware
 
-* [ ] Add CAD to repo
-* [ ] Order 80/20
-* [ ] Spec dampers
-* [ ] Get 8bitdo controller working
+* [X] Order 80/20
+* [X] Spec dampers
+* [X] Get 8bitdo controller talking
  
  ```sudo nano /etc/udev/rules.d/99-8bitdo-xinput.rules ```
 
    ```ACTION=="add", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="3106", RUN+="/sbin/modprobe xpad", RUN+="/bin/sh -c 'echo 2dc8 3106 > /sys/bus/usb/drivers/xpad/new_id'"```
+
+* [ ] Tune damper tension for platform weight
+* [ ] Replace PSU with LIPOs
+* [ ] Add CAD to repo
+* [ ] CAD electronics enclosure for odrive + cpu + batteries
+* [ ] Spec sendcutsend sheet metal enclosure
+ 
---
+++ src/cmd_vel_to_odrive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+import odrive
+from odrive.enums import*
+
+class cmd2odrv():
+
+    def __init__(self, point_cloud_topic='/velodyne_points'): #'/os1_cloud_node/points'
+
+        #init ODrive
+        serial0 = "206A337B304B"
+        self.odrv0 = odrive.find_any(serial_number=serial0)
+        print("found od0")
+        # odrv0.axis0.controller.config.control_mode = 2
+        # odrv0.axis1.controller.config.control_mode = 2
+
+        # Initialize the ROS node
+        rospy.init_node('cmd_vel_listener', anonymous=True)
+        # Subscribe to the 'cmd_vel' topic, which publishes Twist messages
+        rospy.Subscriber("cmd_vel", Twist, self.callback)
+
+    def callback(self, msg):
+        # Extract and print the x-component of linear velocity
+        rospy.loginfo("Linear x velocity: %f, Angular z velocity: %f", msg.linear.x, msg.angular.z)    
+
+        scale = 1000
+
+        L_wheel_setpoint = scale*(msg.linear.x + msg.angular.z)
+        R_wheel_setpoint = -scale*(msg.linear.x - msg.angular.z)
+
+        self.odrv0.axis0.controller.vel_setpoint = L_wheel_setpoint
+        self.odrv0.axis1.controller.vel_setpoint = R_wheel_setpoint
+
+if __name__ == '__main__':
+    cmd_vel_to_odriv = cmd2odrv()
+
+    while not rospy.is_shutdown():
+        rospy.spin()
