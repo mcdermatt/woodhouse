89e57e6b3e712314fccbc4fc663a4fd4c34a58f9
mcdermatt <mcdermatt@gmail.com>
2024-11-30 14:28:27 -0500

    using ICET tf broadcaster to select keyframes at which to save scan context

--- src/scan_context_keyframe_grabber.cpp
+++ src/scan_context_keyframe_grabber.cpp
@@ -6,13 +6,17 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>  // Add this line for tf2_ros::Buffer
 #include <pcl/registration/icp.h>
 #include <Eigen/Dense>
 #include <random>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <tf/tf.h>
+#include <tf2/exceptions.h>
+#include <cmath>
 
 using namespace std;
 using namespace Eigen;
@@ -20,12 +24,17 @@ using namespace Eigen;
 
 class ScanContextNode {
 public:
-    ScanContextNode() : nh_("~"), initialized_(false) {
+    ScanContextNode() : nh_("~"), initialized_(false), tfListener_(tfBuffer_) {
         // Set up ROS subscribers and publishers
         pointcloud_sub_ = nh_.subscribe("/velodyne_points", 10, &ScanContextNode::pointcloudCallback, this); //use when connected to Velodyne VLP-16
         // odom_pub = nh_.advertise<nav_msgs::Odometry>("/odom", 50);
 
         ros::Rate rate(10);
+        pose_at_last_kf.resize(3);
+        pose_at_last_kf << 0., 0., 0.;
+        rot_.w = 1.0; //needed for quat initialization
+
+
     }
 
     void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg) {
@@ -36,27 +45,115 @@ public:
         // Convert PointCloud2 to PCL PointCloud
         pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::fromROSMsg(*msg, *pcl_cloud);
-
         // Convert PCL PointCloud to Eigen::MatrixXf
-        Eigen::MatrixXf pcl_matrix = convertPCLtoEigen(pcl_cloud);
+        // Eigen::MatrixXf pcl_matrix = convertPCLtoEigen(pcl_cloud);
 
- 
-        auto after1 = std::chrono::system_clock::now();
-        auto after1Ms = std::chrono::time_point_cast<std::chrono::milliseconds>(after1);
-        auto elapsedTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(after1Ms - beforeMs).count();
-        std::cout << "Estimated scan context in: " << elapsedTimeMs << " ms" << std::endl;
+        // Convert pcl::PointXYZ to pcl::PointXYZI
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud_intensity(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl_cloud_intensity->reserve(pcl_cloud->size());
+        for (const auto& point : pcl_cloud->points) {
+            pcl::PointXYZI point_intensity;
+            point_intensity.x = point.x;
+            point_intensity.y = point.y;
+            point_intensity.z = point.z;
+            point_intensity.intensity = 0.0f; // Assign default intensity
+            pcl_cloud_intensity->points.push_back(point_intensity);
+        }
+
+        // get overall odometry estimates from ICET
+        try {
+            // Wait for the transform to become available
+            ros::Duration timeout(4.0);
+            if (!tfBuffer_.canTransform("map", "sensor", ros::Time(0), timeout)) {
+                ROS_ERROR("Transform from 'map' to 'sensor' not available within the timeout.");
+            }
+
+            // Look up the transform
+            geometry_msgs::TransformStamped transformStamped = tfBuffer_.lookupTransform("map", "sensor", ros::Time(0));
+
+            // Extract translation and rotation
+            trans_ = transformStamped.transform.translation;
+            rot_ = transformStamped.transform.rotation;
+            std::cout << "Translation: [" << trans_.x << ", " << trans_.y << ", " << trans_.z << "]" << std::endl;
+            // std::cout << "Rotation: [" << rot_.x << ", " << rot_.y << ", " << rot_.z << ", " << rot_.w << "]" << std::endl;
+
+        } catch (const tf2::TransformException& ex) {
+            ROS_ERROR("Transform error: %s", ex.what());
+        }
+        frames_since_last_kf++;
+        cout<< frames_since_last_kf << " " << pose_at_last_kf << endl;        
+
+        dist_since_last_kf = sqrt(pow(pose_at_last_kf[0] - trans_.x, 2) 
+                                + pow(pose_at_last_kf[1] - trans_.y, 2) 
+                                + pow(pose_at_last_kf[2] - trans_.z, 2));
+
+        std::cout << "dist since last kf " << dist_since_last_kf << endl;
+
+        if (dist_since_last_kf > dist_thresh && frames_since_last_kf > frame_thresh){
+
+            // Compute the scan context descriptor
+            sc_manager_.makeAndSaveScancontextAndKeys(*pcl_cloud_intensity);
+            ROS_INFO("Scan context generated and saved!");
+
+            const Eigen::MatrixXd& latest_context = sc_manager_.polarcontexts_.back();
+
+            pose_at_last_kf[0] = static_cast<float>(trans_.x);
+            pose_at_last_kf[1] = static_cast<float>(trans_.y);
+            pose_at_last_kf[2] = static_cast<float>(trans_.z);
+            frames_since_last_kf = 0;
+
+            auto after1 = std::chrono::system_clock::now();
+            auto after1Ms = std::chrono::time_point_cast<std::chrono::milliseconds>(after1);
+            auto elapsedTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(after1Ms - beforeMs).count();
+            std::cout << "Estimated scan context in: " << elapsedTimeMs << " ms" << std::endl;
+
+        }
+
+
+        // std::cout << "Latest Scan Context:\n" << latest_context << "\n";
+
+        // //save csv
+        // std::ofstream file("latest_scan_context.csv");
+        // if (file.is_open()) {
+        //     file << latest_context.format(Eigen::IOFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ",", "\n"));
+        //     file.close();
+        // }
+
+        // //display with opencv
+        // cv::Mat context_image(latest_context.rows(), latest_context.cols(), CV_64F, const_cast<double*>(latest_context.data()));
+        // cv::normalize(context_image, context_image, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+        // cv::Mat resized_image;
+        // int scale_factor = 10; // Increase this for larger size
+        // cv::resize(context_image, resized_image, cv::Size(), scale_factor, scale_factor, cv::INTER_NEAREST);
+        // // Display the resized image
+        // cv::imshow("Scan Context", resized_image);
+        // cv::waitKey(0);
+
+
     }
 
     Eigen::VectorXf X0;
 
 private:
+    const float dist_thresh = 2.0;
+    const int frame_thresh = 10;
+
     ros::NodeHandle nh_;
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformListener tfListener_;
     ros::Subscriber pointcloud_sub_;
-    // ros::Publisher odom_pub;
+    SCManager sc_manager_; // ScanContext manager instance
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr prev_pcl_cloud_;
     bool initialized_;
     int frameCount = 0;
+    int frames_since_last_kf = 0;
+    float dist_since_last_kf = 0.;
+    Eigen::VectorXf pose_at_last_kf;
     Eigen::MatrixXf prev_pcl_matrix;
+
+    geometry_msgs::Vector3 trans_ = {};   // for translation (x, y, z)
+    geometry_msgs::Quaternion rot_ = {};  // for rotation (x, y, z, w)
 
     //init variable to hold cumulative homogenous transform
     Eigen::Matrix4f X_homo = Eigen::Matrix4f::Identity(); 
